883e97e10330e4ba50516e36d31f263f2c4e7bf1
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-25 22:14:23 +0700

    menambahkan fragment 1

--- app/src/main/java/com/ihfazh/latihanfragment/MainActivity.java
+++ app/src/main/java/com/ihfazh/latihanfragment/MainActivity.java
@@ -1,14 +1,31 @@
 package com.ihfazh.latihanfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager mFragmentManager;
+    private homeFragment mHomeFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFragmentManager = getSupportFragmentManager();
+        mHomeFragment = new homeFragment();
+        Fragment fragment = mFragmentManager.findFragmentByTag(homeFragment.class.getSimpleName());
+
+        if (!(fragment instanceof homeFragment)){
+            mFragmentManager.beginTransaction()
+                    .add(R.id.frame_container, mHomeFragment, homeFragment.class.getSimpleName())
+                    .commit();
+
+        }
+
     }
 }
