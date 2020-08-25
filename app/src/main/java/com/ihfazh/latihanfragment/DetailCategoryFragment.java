0d07ac1461135b8ac01bf67bac20b036ea04ffd7
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2020-08-25 22:48:18 +0700

    menambahkan fragment 3

--- app/src/main/java/com/ihfazh/latihanfragment/DetailCategoryFragment.java
+++ app/src/main/java/com/ihfazh/latihanfragment/DetailCategoryFragment.java
@@ -5,19 +5,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CategoryFragment#newInstance} factory method to
+ * Use the {@link DetailCategoryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CategoryFragment extends Fragment {
+public class DetailCategoryFragment extends Fragment {
+
+    public static String EXTRA_NAME = "extra_name";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,9 +30,12 @@ public class CategoryFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private Button to_lifestyle;
+    private TextView tvName, tvDescription;
+    private Button btnToProfile, btnShowDialog;
 
-    public CategoryFragment() {
+    private String description = "";
+
+    public DetailCategoryFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +45,11 @@ public class CategoryFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CategoryFragment.
+     * @return A new instance of fragment DetailCategoryFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CategoryFragment newInstance(String param1, String param2) {
-        CategoryFragment fragment = new CategoryFragment();
+    public static DetailCategoryFragment newInstance(String param1, String param2) {
+        DetailCategoryFragment fragment = new DetailCategoryFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,34 +70,46 @@ public class CategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        return inflater.inflate(R.layout.fragment_detail_category, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        to_lifestyle = view.findViewById(R.id.to_lifestyle);
-        to_lifestyle.setOnClickListener(new View.OnClickListener() {
+
+        tvDescription = view.findViewById(R.id.tv_description);
+        tvName = view.findViewById(R.id.tv_category);
+
+        String categoryName = getArguments().getString(EXTRA_NAME);
+        tvName.setText(categoryName);
+
+        tvDescription.setText(getDescription());
+
+        btnShowDialog = view.findViewById(R.id.open_popup);
+        btnToProfile = view.findViewById(R.id.to_profile);
+
+        btnShowDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentManager fragmentManager = getFragmentManager();
 
-                DetailCategoryFragment detailCategoryFragment = new DetailCategoryFragment();
-                Bundle bundle = new Bundle();
-
-                bundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle");
-                detailCategoryFragment.setArguments(bundle);
-
-                detailCategoryFragment.setDescription("Ini adalah Deskripsi dari produk lifestyle.");
-
-                if (fragmentManager != null){
-                    fragmentManager
-                            .beginTransaction()
-                            .replace(R.id.frame_container, detailCategoryFragment, DetailCategoryFragment.class.getSimpleName())
-                            .addToBackStack(null)
-                            .commit();
-                }
             }
         });
+
+        btnToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
